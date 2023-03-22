30f8e7eae194604ab8f711f287ce492f82ffc2c5
mskothari07 <kotharimeet07@gmail.com>
2023-03-22 12:35:51 +0530

    login and register api

---
+++ api/controllers/auth.js
@@ -0,0 +1,48 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import { createError } from "../utils/error.js";
+
+export const register = async (req, res, next) => {
+    try {
+        //Password Hash
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hash,
+        });
+
+        await newUser.save();
+        res.status(201).json({
+            message: "User created successfully",
+            data: newUser,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const login = async (req, res, next) => {
+    try {
+        //finding the username
+        const user = await User.findOne({ username: req.body.username });
+        if (!user) return next(createError(404, "User not found"));
+
+        // Checking the password
+        const isPasswordCorrect = await bcrypt.compare(
+            req.body.password,
+            user.password
+        );
+        if (!isPasswordCorrect)
+            return next(createError(400, "Inccorrect password"));
+
+        //Retruning other details expect password
+        const { password, isAdmin, ...otherDetails } = user._doc;
+
+        res.status(200).json({ ...otherDetails });
+    } catch (error) {
+        next(error);
+    }
+};
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,65 +1,26 @@
 import express from "express";
-import Hotel from "../models/Hotel.js";
-import { createError } from "../utils/error.js";
+import {
+  createHotel,
+  deleteHotel,
+  getAllHotel,
+  getHotel,
+  updateHotel,
+} from "../controllers/hotel.js";
 
 const router = express.Router();
 
 // Create
-router.post("/", async (req, res) => {
-  const newHotel = new Hotel(req.body);
-
-  try {
-    const savedHotel = await newHotel.save();
-    res.status(200).json(savedHotel);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
+router.post("/", createHotel);
 // Update
-router.put("/:id", async (req, res) => {
-  try {
-    const updatedHotel = await Hotel.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    );
-    res.status(200).json(updatedHotel);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+router.put("/:id", updateHotel);
 
 // Delete
-router.delete("/:id", async (req, res) => {
-  try {
-    await Hotel.findByIdAndDelete(req.params.id);
-    res.status(200).json("Your hotel has been deleted !!");
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+router.delete("/:id", deleteHotel);
 
 // Get Specific Hotel
-router.get("/:id", async (req, res) => {
-  try {
-    const hotel = await Hotel.findById(req.params.id);
-    res.status(200).json(hotel);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+router.get("/:id", getHotel);
 
 // Get all the Hotel
-router.get("/", async (req, res, next) => {
-  try {
-    const hotels = await Hotel.find();
-    res.status(200).json(hotels);
-  } catch (err) {
-    next(err);
-  }
-});
+router.get("/", getAllHotel);
 
 export default router;
