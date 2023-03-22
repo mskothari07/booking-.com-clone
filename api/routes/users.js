6779b6244df4902bd9edd5fe356485a8f85c9856
mskothari07 <kotharimeet07@gmail.com>
2023-03-22 18:19:47 +0530

    JWT added

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,22 @@
 import express from "express";
+import { deleteUser, getAllUser, getUser, updateUser } from "../controllers/user.js";
 
 const router = express.Router();
 
+
+// Update
+router.put("/:id", updateUser);
+
+// Delete
+router.delete("/:id", deleteUser);
+
+// Get Specific User
+router.get("/:id", getUser);
+
+// Get all the User
+router.get("/", getAllUser);
+
 export default router;
+
+
+
