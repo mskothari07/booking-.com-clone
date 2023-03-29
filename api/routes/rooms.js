f978faec227567ac7679aca7f4b620c77af4ef30
mskothari07 <kotharimeet07@gmail.com>
2023-03-29 08:32:50 +0530

    Room routes added

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,27 @@
 import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getAllRoom,
+  getRoom,
+  updateRoom,
+} from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verfiytoken.js";
 
 const router = express.Router();
+
+// Create
+router.post("/:hotelid", verifyAdmin, createRoom);
+// Update
+router.put("/:id", verifyAdmin, updateRoom);
+
+// Delete
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+
+// Get Specific Room
+router.get("/:id", getRoom);
+
+// Get all the Room
+router.get("/", getAllRoom);
 
 export default router;
