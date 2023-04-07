d6ad655c7c14b51e1b32be9f99011d290b80d40e
mskothari07 <kotharimeet07@gmail.com>
2023-04-07 05:39:41 +0530

    Navbar created

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -1,10 +1,17 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import List from "./pages/list/List";
+import Hotel from "./pages/hotel/Hotel";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/hotels" element={<List />}></Route>
+        <Route path="/hotels/:id" element={<Hotel />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
