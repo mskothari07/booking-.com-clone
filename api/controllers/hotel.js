de600debfe6e7c1f51efdca46a711b1148b577e3
mskothari07 <kotharimeet07@gmail.com>
2023-04-16 18:05:08 +0530

    Added getbycount i.e property count controller

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -53,24 +53,34 @@ export const getAllHotel = async (req, res, next) => {
   }
 };
 
-export const countByType = async (req, res, next) => {
-  try {
-    const hotels = await Hotel.find();
-    res.status(200).json(hotels);
-  } catch (err) {
-    next(err);
-  }
-};
 
 export const countByCity = async (req, res, next) => {
-
   const cities = req.query.cities.split(",")
-
   try {
     const list = await Promise.all(cities.map(city=>{
       return Hotel.countDocuments({city:city})
     }))
     res.status(200).json(list);
+  } catch (err) {
+    next(err);
+  }
+};
+
+
+export const countByType = async (req, res, next) => {
+  try {
+    const hotelCount =  await Hotel.countDocuments({ type:"Hotel"})
+    const apartmentCount =  await Hotel.countDocuments({ type:"Apartment"})
+    const resortCount = await  Hotel.countDocuments({ type:"Resort"})
+    const villaCount =  await Hotel.countDocuments({ type:"Villa"})
+
+   
+    res.status(200).json([
+      {type:"Hotel", count:hotelCount},
+      {type:"Apartment", count:apartmentCount},
+      {type:"Resort", count:resortCount},
+      {type:"Villa", count:villaCount},
+    ]);
   } catch (err) {
     next(err);
   }
